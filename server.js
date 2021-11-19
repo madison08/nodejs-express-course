c33dfd8059614cf269fc449b18b706da0503a449
madison <madisonamoa@gmail.com>
2021-11-19 10:06:51 +0000

    creating a model schema

--- server.js
+++ server.js
@@ -9,6 +9,24 @@ mongoose.connect('mongodb://localhost:27017/natours-app').then((res) => {
     console.log('[database] is running')
 })
 
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: String,
+        default: 4.5
+    },
+    price: {
+        type: String,
+        required: [true, 'A tour must have a price']
+    }
+})
+
+const Tour = mongoose.model('Tour', tourSchema)
+
 app.listen('3200', () => {
 
     console.log('[server] running on http://localhost:3200')
