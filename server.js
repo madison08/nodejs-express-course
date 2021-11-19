4cd880c5d83532945a44eb165f43e333789a35c8
madison <madisonamoa@gmail.com>
2021-11-19 07:47:32 +0000

    connecting our database with express app

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
+const mongoose = require('mongoose')
+
 const app = require('./app')
 
 
 // console.log(process.env)
 
+mongoose.connect('mongodb://localhost:27017/natours-app').then((res) => {
+    console.log('[database] is running')
+})
+
 app.listen('3200', () => {
 
     console.log('[server] running on http://localhost:3200')
 })
+
