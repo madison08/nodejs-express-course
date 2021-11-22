3ef36e0c51d0cb7d97933019e4f82a4f8c231e24
madison <madisonamoa@gmail.com>
2021-11-22 14:14:01 +0000

    deleted document

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -147,15 +147,19 @@ exports.updateTour = async (req, res) => {
 
 }
 
-exports.deleteTour = (req, res) =>{
+exports.deleteTour = async (req, res) =>{
 
-    // const id = req.params.id * 1
-    // const tour = tours.find((tours) => tours.id === id)
+    try{
 
-    if(!tour){
-        return res.status(404).json({
-            status: "fail",
-            message: "deleted"
+        const tour = await Tour.findByIdAndDelete(req.params.id)
+
+        res.status(200).json({
+            status: "success",
+            message: null
         })
+
+    }catch(err){
+        console.log(err)
     }
+
 }
