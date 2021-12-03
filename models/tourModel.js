df8ce868996911770ae6af5104b1e88fd59edbea
madison <madisonamoa@gmail.com>
2021-12-03 10:28:13 +0000

    data validation in model

--- models/tourModel.js
+++ models/tourModel.js
@@ -5,7 +5,9 @@ const tourSchema = new mongoose.Schema({
     name: {
         type: String,
         required: [true, 'A tour must have a name'],
-        unique: true
+        unique: true,
+        maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+        minlength: [10, 'A tour name must have more or equal then 10 characters']
     },
     slug: String,
     duration:{
@@ -18,11 +20,17 @@ const tourSchema = new mongoose.Schema({
     },
     difficulty:{
         type: String,
-        required: [true, 'A tour must have a difficulty']
+        required: [true, 'A tour must have a difficulty'],
+        enum: {
+            values: ['easy', 'medium', 'difficult'],
+            message: 'Difficulty is either: easy, medium, difficult'
+        }
     },
     ratingsAverage: {
         type: Number,
-        default: 4.5
+        default: 4.5,
+        min: [1, 'Rating must have be above 1.0'],
+        max: [5, 'Rating must have be below 5.0']
     },
     ratingsQuantity:{
         type: Number,
