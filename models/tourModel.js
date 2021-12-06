7268a52d6d26dc056c67fef6f11478a07c66642c
madison <madisonamoa@gmail.com>
2021-12-06 07:23:07 +0000

    data custom validation

--- models/tourModel.js
+++ models/tourModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
 const slugify = require('slugify')
+const validator = require('validator')
 
 const tourSchema = new mongoose.Schema({
     name: {
@@ -7,7 +8,8 @@ const tourSchema = new mongoose.Schema({
         required: [true, 'A tour must have a name'],
         unique: true,
         maxlength: [40, 'A tour name must have less or equal then 40 characters'],
-        minlength: [10, 'A tour name must have more or equal then 10 characters']
+        minlength: [10, 'A tour name must have more or equal then 10 characters'],
+        // validate: [validator.isAlpha, 'Tour name must only contain characters']
     },
     slug: String,
     duration:{
@@ -40,7 +42,15 @@ const tourSchema = new mongoose.Schema({
         type: String,
         required: [true, 'A tour must have a price']
     },
-    priceDiscount: Number,
+    priceDiscount: {
+        type: Number,
+        validate: {
+            validator: function(val){
+                return val < this.price; //
+            },
+            message: 'Discount price ({VALUE}) should be below regular price'
+        }
+    },
     summary:{
         type: String,
         trim: true,
