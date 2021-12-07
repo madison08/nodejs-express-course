90f8ab301f208711961b514c96cd43f85253a208
madison <madisonamoa@gmail.com>
2021-12-07 15:59:12 +0000

    Global handling error

--- app.js
+++ app.js
@@ -31,12 +31,34 @@ app.use('/api/v1/users', userRouter)
 
 app.all('*', (req, res, next) => {
 
-    res.status(404).json({
-        status: 'fail',
-        message: `can't find ${req.originalUrl}`
-    })
+    // res.status(404).json({
+    //     status: 'fail',
+    //     message: `can't find ${req.originalUrl}`
+    // })
 
+    const err = new Error(`can't find ${req.originalUrl}`)
+
+    err.status = 'fail';
+    err.statusCode = 404;
+
+    next(err)
 })
+
+app.use((err, req, res, next) => {
+
+    err.statusCode = err.statusCode || 500;
+
+    err.status = err.status || 'error'
+
+    res.status(err.statusCode).json({
+
+        status: err.status,
+        message: err.message
+
+    })
+})
+
+
 
 
 
